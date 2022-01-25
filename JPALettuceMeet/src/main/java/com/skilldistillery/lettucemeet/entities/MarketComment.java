a919ae20a6ae74f323aa525d8d3ea2d3e03abe58
Gramman87 <gramman87@gmail.com>
2022-01-25 16:13:18 -0700

    partial entity buildout, no JUnit complete

---
+++ JPALettuceMeet/src/main/java/com/skilldistillery/lettucemeet/entities/MarketComment.java
@@ -0,0 +1,92 @@
+package com.skilldistillery.lettucemeet.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "market_comment")
+public class MarketComment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String comment;
+	
+	@CreationTimestamp
+	@Column(name = "created_time")
+	private LocalDateTime created;
+	
+	@UpdateTimestamp
+	@Column(name = "updated_time")
+	private LocalDateTime updated;
+
+	public MarketComment() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarketComment other = (MarketComment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "MarketComment [id=" + id + ", created=" + created + ", updated=" + updated + "]";
+	}
+	
+}
---
+++ JPALettuceMeet/src/main/java/com/skilldistillery/lettucemeet/entities/ProductComment.java
@@ -0,0 +1,92 @@
+package com.skilldistillery.lettucemeet.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "product_comment")
+public class ProductComment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String comment;
+	
+	@CreationTimestamp
+	@Column(name = "create_time")
+	private LocalDateTime created;
+	
+	@UpdateTimestamp
+	@Column(name = "update_time")
+	private LocalDateTime updated;
+
+	public ProductComment() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductComment other = (ProductComment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductComment [id=" + id + ", created=" + created + ", updated=" + updated + "]";
+	}
+	
+}
