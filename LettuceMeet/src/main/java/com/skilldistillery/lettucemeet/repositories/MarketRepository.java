4ca3291d6b7a227489d2c7c6a515420996ca0be8
ai24m <powellallysa@gmail.com>
2022-01-26 11:11:20 -0700

    market repo update

---
+++ LettuceMeet/src/main/java/com/skilldistillery/lettucemeet/repositories/MarketRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.lettucemeet.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.lettucemeet.entities.Market;
+
+public interface MarketRepository extends JpaRepository<Market, Integer> {
+
+}
---
+++ LettuceMeet/src/main/java/com/skilldistillery/lettucemeet/services/MarketServiceImpl.java
@@ -0,0 +1,64 @@
+package com.skilldistillery.lettucemeet.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.lettucemeet.entities.Address;
+import com.skilldistillery.lettucemeet.entities.Market;
+import com.skilldistillery.lettucemeet.entities.User;
+import com.skilldistillery.lettucemeet.repositories.AddressRepository;
+import com.skilldistillery.lettucemeet.repositories.MarketRepository;
+import com.skilldistillery.lettucemeet.repositories.UserRepository;
+
+public class MarketServiceImpl implements MarketService {
+
+	@Autowired 
+	private MarketRepository marketRepo;
+	
+	@Autowired 
+	private UserRepository userRepo;
+	
+	@Autowired 
+	private AddressRepository addressRepo;
+	
+	@Override
+	public List<Market> index() {
+		return marketRepo.findAll();
+	} 
+	
+	@Override
+	public Market show(int mId) {
+		Optional<Market> mOpt = marketRepo.findById(mId); 
+		if (mOpt.isPresent()) {
+			Market market = mOpt.get(); 
+			return market; 
+		} return null;
+	}
+
+	@Override
+	public Market create(Market market, Address address, User user) {
+		addressRepo.saveAndFlush(address); 
+		userRepo.saveAndFlush(user); 
+		market.setAddress(address);
+		market.setUser(user);
+		marketRepo.saveAndFlush(market); 
+		return market;
+	}
+
+	@Override
+	public Market update(User user, Integer mId, Market market) {
+		if (market.getUser().getId() == user.getId()) {
+			Optional<Market> mOpt = marketRepo.findById(market.getId());
+			if (mOpt.isPresent()) {
+				Market updatedMarket = mOpt.get();
+				updatedMarket.setAddress(market.getAddress());
+			}
+		}
+		return null;
+	}
+
+
+	
+}
