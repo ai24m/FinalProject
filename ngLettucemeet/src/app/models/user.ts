141599adf9ff5fe4d60f7373a39328ae2b54082f
Chris Delong <admin@frosthaven.net>
2022-01-28 15:28:10 -0700

    Login route

--- ngLettucemeet/src/app/models/user.ts
+++ ngLettucemeet/src/app/models/user.ts
@@ -1,2 +1,28 @@
+import { Address } from "./address";
+
 export class User {
+  id: number;
+  username: string;
+  password: string;
+  email: string | undefined;
+  role: string | undefined;
+  firstname: string;
+  lastname: string;
+  businessname: string;
+  imageurl: string;
+  address: Address | undefined;
+
+  constructor(id: number = 0, username: string = '', password: string = '', email?: string, role?: string,
+  firstname: string = '', lastname: string = '', businessname: string = '', imageurl: string = '', address?: Address) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    this.role = role;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.businessname = businessname;
+    this.imageurl = imageurl;
+    this.address = address;
+  }
 }
