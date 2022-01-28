d44b09c2ea999b9bf0683bce625c86be3d4920e4
ai24m <powellallysa@gmail.com>
2022-01-28 08:59:33 -0700

    angular layout

---
+++ ngLettucemeet/src/app/components/sample-template/sample-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-template',
+  templateUrl: './sample-template.component.html',
+  styleUrls: ['./sample-template.component.css']
+})
+export class SampleTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
