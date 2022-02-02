bfd9894f801f82a0b7faf1c7e9bfe20fad81b29f
Chris Delong <admin@frosthaven.net>
2022-02-01 17:03:32 -0700

    hopefully this fixes everything

--- LettuceMeet/src/main/java/com/skilldistillery/lettucemeet/security/SecurityConfig.java
+++ LettuceMeet/src/main/java/com/skilldistillery/lettucemeet/security/SecurityConfig.java
@@ -28,6 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // For CORS, the preflight request
 				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll() // will hit the OPTIONS on the route
+//				.antMatchers(HttpMethod.POST, "/api/users/").permitAll() // will hit the OPTIONS on the route
+//				.antMatchers(HttpMethod.POST, "/api/login/").permitAll() // will hit the OPTIONS on the route
+//				.antMatchers(HttpMethod.POST, "/api/users").permitAll() // will hit the OPTIONS on the route
 				.antMatchers(HttpMethod.OPTIONS, "/api/user").hasAuthority("admin") // will hit the OPTIONS on the route
 				.antMatchers(HttpMethod.OPTIONS, "/api/users").hasAuthority("admin") // will hit the OPTIONS on the route
 				.antMatchers(HttpMethod.POST, "/api/users/").permitAll() // will hit the OPTIONS on the route
@@ -43,16 +46,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, "/api/productRatings/").permitAll() // will hit the OPTIONS on the route
 				.antMatchers(HttpMethod.GET, "/api/productratings/**").permitAll() // will hit the OPTIONS on the route
 				.antMatchers(HttpMethod.GET, "/api/marketcomments/**").permitAll() // will hit the OPTIONS on the route
-				.antMatchers(HttpMethod.GET, "/api/products").permitAll() // will hit the OPTIONS on the route
-				.antMatchers(HttpMethod.GET, "/api/products/**").permitAll() // will hit the OPTIONS on the route
 				.antMatchers(HttpMethod.GET, "/api/market/**").permitAll() 
-				.antMatchers(HttpMethod.GET, "/api/marketcomments").permitAll() 
+//				.antMatchers(HttpMethod.PUT, "/api/markets/**").hasAuthority("user")
 				.antMatchers(HttpMethod.GET, "/api/sellerRatings/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/api/marketratings/**").permitAll() 
 				.antMatchers(HttpMethod.GET, "/api/products").permitAll() 
 				.antMatchers(HttpMethod.GET, "/api/products/**").permitAll() 
 				.antMatchers(HttpMethod.GET, "/api/productcomments").permitAll() 
 				.antMatchers(HttpMethod.GET, "/api/productcomments/**").permitAll() 
+//				.antMatchers(HttpMethod.DELETE, "/api/productcomments/**").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/marketratings/avegrating/**").permitAll() 
+//				.antMatchers(HttpMethod.PUT, "/api/users/**").hasAuthority("user")
+//				.antMatchers(HttpMethod.POST, "/api/productcomments/**").permitAll() 
+//				.antMatchers(HttpMethod.PUT, "/api/productcomments/**").permitAll() 
+//				.antMatchers(HttpMethod.DELETE, "/api/marketcomments/**").hasAuthority("user")
+//				.antMatchers(HttpMethod.OPTIONS, "/api/users/*").hasAuthority("admin") // return true or false 
+//				.antMatchers(HttpMethod.POST, "/api/**").hasAuthority("user") // return true or false 
+//				.antMatchers(HttpMethod.PUT, "/api/**").hasAuthority("user") // return true or false 
+//				.antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority("user") // return true or false 
+				.antMatchers("/api/**").hasAuthority("admin") // return true or false 
+//				.antMatchers(HttpMethod.PUT, "/api/**").hasAuthority("user") // return true or false 
+//				.antMatchers(HttpMethod.POST, "/api/**").hasAuthority("user") // return true or false 
 				.antMatchers(HttpMethod.OPTIONS, "/api/users/**").hasAuthority("admin") // return true or false 
 				.antMatchers(HttpMethod.PUT, "/api/**").hasAuthority("standard") // return true or false 
 				.antMatchers(HttpMethod.POST, "/api/**").hasAuthority("standard") // return true or false 
